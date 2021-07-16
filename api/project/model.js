bfdd70abb9e242d2cabe0ee5870f2bb89dee6a21
davedwight <dpdwight@gmail.com>
2021-07-16 14:18:29 -0600

    display boolean to client

--- api/project/model.js
+++ api/project/model.js
@@ -2,7 +2,23 @@ const db = require("../../data/dbConfig");
 
 async function get() {
   const rows = await db("projects as p");
-  return rows;
+
+  const result = [];
+
+  rows.forEach((row) => {
+    if (row.project_completed === 0) {
+      result.push({
+        ...row,
+        project_completed: false,
+      });
+    } else {
+      result.push({
+        ...row,
+        project_completed: true,
+      });
+    }
+  });
+  return result;
 }
 
 module.exports = {
