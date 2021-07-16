80fcda81ff71197920855b013af64c0aeeba3802
davedwight <dpdwight@gmail.com>
2021-07-16 12:56:49 -0600

    build migration

--- api/server.js
+++ api/server.js
@@ -1,4 +1,3 @@
-// build your server here and require it from index.js
 const express = require("express");
 const projectRouter = require("./project/router");
 const resourceRouter = require("./resource/router");
